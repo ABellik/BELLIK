045a8513207f223b98c3f64a1df3d8d58e7f3e88
Adam <admblk04@gmail.com>
2025-04-21 10:10:11 +0200

    21/04

---
+++ src/exceptions/IllegalPercentageException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class IllegalPercentageException extends IllegalArgumentException {
+    public IllegalPercentageException(String message) {
+        super(message);
+    }
+}
